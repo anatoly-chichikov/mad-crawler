c8018d512d7ccad4d1c17f1f7ba7a86ff58c99a9
Anatoly Chichikov <treks.mbox@gmail.com>
2014-08-21 16:44:28 +0400

    Project level exception

---
+++ src/main/java/madcrawler/files/TargetsReader.java
@@ -0,0 +1,10 @@
+package madcrawler.files;
+
+import java.net.URL;
+import java.util.Set;
+
+public class TargetsReader {
+    public Set<URL> getUrlsFromFile(String path) {
+        return null;
+    }
+}
---
+++ src/main/java/madcrawler/settings/CrawlerException.java
@@ -0,0 +1,8 @@
+package madcrawler.settings;
+
+public class CrawlerException extends RuntimeException {
+
+    public CrawlerException(String message) {
+        super(message);
+    }
+}
