5d5db7399df2262f1e8d2056ad323095383cbb5b
Anatoly Chichikov <treks.mbox@gmail.com>
2014-08-21 15:45:45 +0400

    Dependencies and app stub

---
+++ src/main/java/madcrawler/App.java
@@ -0,0 +1,14 @@
+package madcrawler;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Mad Crawler app has been started");
+        new App().start();
+        System.out.println("Mad Crawler app has been finished");
+    }
+
+    private void start() {
+        System.out.println("Doing work...");
+    }
+}
---
+++ src/test/java/madcrawler/AppTest.java
@@ -0,0 +1,11 @@
+package madcrawler;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testStart() {
+        App.main(new String[]{});
+    }
+}
