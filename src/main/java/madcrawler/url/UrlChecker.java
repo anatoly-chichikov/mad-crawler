6c4769b6eabb37b949f83cff3d086d0a14cc0a95
Anatoly Chichikov <treks.mbox@gmail.com>
2014-08-22 17:32:22 +0400

    New URL checking methods

--- src/main/java/madcrawler/url/UrlChecker.java
+++ src/main/java/madcrawler/url/UrlChecker.java
@@ -1,8 +1,32 @@
 package madcrawler.url;
 
+import java.net.URI;
+import java.net.URL;
+
+import static com.google.common.base.Objects.equal;
+
 public class UrlChecker {
 
     public boolean hasProtocol(String url) {
-        return url.startsWith("http://");
+        return url.startsWith("http://") || url.startsWith("https://");
+    }
+
+    public boolean isAbsoluteExternal(URI underTest, URL base) {
+        return underTest.isAbsolute() &&
+            !equal(underTest.getHost(), base.getHost());
+    }
+
+    public boolean isAbsoluteInternal(URI underTest, URL base) {
+        return underTest.isAbsolute() &&
+            equal(underTest.getHost(), base.getHost());
+    }
+
+    public boolean isValidProtocol(URI underTest) {
+        return equal(underTest.getScheme(), "http") ||
+            equal(underTest.getScheme(), "https");
+    }
+
+    public boolean isContainsFragment(URI underTest) {
+        return underTest.getFragment() != null;
     }
 }
--- src/test/java/madcrawler/url/UrlCheckerTest.java
+++ src/test/java/madcrawler/url/UrlCheckerTest.java
@@ -2,24 +2,72 @@ package madcrawler.url;
 
 import org.junit.Test;
 
+import java.net.URI;
+import java.net.URL;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UrlCheckerTest {
 
+    UrlChecker checker = new UrlChecker();
+
     @Test
     public void testHasProtocol() {
-        UrlChecker checker = new UrlChecker();
-        String url = "http://test.me";
+        String urlHttp = "http://test.me";
+        String urlHttps = "https://test.me";
+        String urlWithoutProtocol = "test.me";
 
-        assertTrue(checker.hasProtocol(url));
+        assertTrue(checker.hasProtocol(urlHttp));
+        assertTrue(checker.hasProtocol(urlHttps));
+        assertFalse(checker.hasProtocol(urlWithoutProtocol));
     }
 
     @Test
-    public void testWithoutProtocol() {
-        UrlChecker checker = new UrlChecker();
-        String url = "test.me";
+    public void testAbsoluteExternal() throws Exception{
+        URL baseUrl = new URL("http://test.me");
+        URI valid = new URI("http://another.com/page/");
+        URI invalid = new URI("http://test.me/page/");
+        URI internal = new URI("internal/page/");
 
-        assertFalse(checker.hasProtocol(url));
+        assertTrue(checker.isAbsoluteExternal(valid, baseUrl));
+        assertFalse(checker.isAbsoluteExternal(invalid, baseUrl));
+        assertFalse(checker.isAbsoluteExternal(internal, baseUrl));
+    }
+
+    @Test
+    public void testAbsoluteInternal() throws Exception{
+        URL baseUrl = new URL("http://test.me");
+        URI valid = new URI("http://test.me/page/");
+        URI invalid = new URI("http://another.com/page/");
+        URI internal = new URI("internal/page/");
+
+        assertTrue(checker.isAbsoluteInternal(valid, baseUrl));
+        assertFalse(checker.isAbsoluteInternal(invalid, baseUrl));
+        assertFalse(checker.isAbsoluteInternal(internal, baseUrl));
+    }
+
+    @Test
+    public void testValidProtocol() throws Exception{
+        URI http = new URI("http://test.me/page/");
+        URI https = new URI("https://another.com/page/");
+        URI ftp = new URI("ftp://test.me");
+        URI sftp = new URI("sftp://test.me");
+
+        assertTrue(checker.isValidProtocol(http));
+        assertTrue(checker.isValidProtocol(https));
+        assertFalse(checker.isValidProtocol(ftp));
+        assertFalse(checker.isValidProtocol(sftp));
+    }
+
+    @Test
+    public void testFragmentPresence() throws Exception {
+        URI with = new URI("http://test.me/page#link");
+        URI without = new URI("http://test.me/page");
+        URI withParams = new URI("http://test.me/page?one=1&two=2");
+
+        assertTrue(checker.isContainsFragment(with));
+        assertFalse(checker.isContainsFragment(without));
+        assertFalse(checker.isContainsFragment(withParams));
     }
 }
